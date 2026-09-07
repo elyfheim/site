68fe8dc6d6be6bc0968d9463610115cc1754064b
elyfheim <boundlesselation@protonmail.com>
2026-09-07 18:27:21 +0700

    feat: add back to top button and update home content

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,28 @@
 const toggle = document.getElementById("mode-toggle");
-let mode = "dark";
-toggle.addEventListener("click", () => {
-	if (mode === "dark") {
-		mode = "light";
+
+if (!localStorage.getItem("mode-toggle")) {
+	localStorage.setItem("mode-toggle", "dark");
+} else {
+	const mode = localStorage.getItem("mode-toggle");
+	if (mode === "light") {
 		document.body.classList.add("light-body");
 		document.getElementsByTagName("header")[0].classList.add("light-body");
 		document.getElementsByTagName("main")[0].classList.add("light-text");
 	} else {
-		mode = "dark";
+		document.body.classList.remove("light-body");
+		document.getElementsByTagName("header")[0].classList.remove("light-body");
+		document.getElementsByTagName("main")[0].classList.remove("light-text");
+	}
+}
+toggle.addEventListener("click", () => {
+	const mode = localStorage.getItem("mode-toggle");
+	if (mode === "dark") {
+		localStorage.setItem("mode-toggle", "light");
+		document.body.classList.add("light-body");
+		document.getElementsByTagName("header")[0].classList.add("light-body");
+		document.getElementsByTagName("main")[0].classList.add("light-text");
+	} else {
+		localStorage.setItem("mode-toggle", "dark");
 		document.body.classList.remove("light-body");
 		document.getElementsByTagName("header")[0].classList.remove("light-body");
 		document.getElementsByTagName("main")[0].classList.remove("light-text");
